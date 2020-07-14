469719aedf052606bb2f8e563e2007ef992292e5
a.romanov <a.romanov@rbkmoney.com>
2020-07-14 12:27:04 +0300

    BJ-960: Initial commit

---
+++ src/test/java/com/rbkmoney/bukiper/BukiperApplicationTest.java
@@ -0,0 +1,16 @@
+package com.rbkmoney.bukiper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BukiperApplication.class)
+public class BukiperApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
