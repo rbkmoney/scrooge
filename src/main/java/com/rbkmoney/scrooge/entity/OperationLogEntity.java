02c3073dd8b0ffc1fa5ccc6810e74975f762f783
a.romanov <a.romanov@rbkmoney.com>
2020-07-21 14:12:06 +0300

    BJ-960: Provide tests

--- src/main/java/com/rbkmoney/scrooge/entity/OperationLogEntity.java
+++ src/main/java/com/rbkmoney/scrooge/entity/OperationLogEntity.java
@@ -2,14 +2,15 @@ package com.rbkmoney.scrooge.entity;
 
 import com.rbkmoney.scrooge.entity.enums.OperationType;
 import com.rbkmoney.scrooge.entity.enums.ValidationError;
+import com.rbkmoney.scrooge.entity.naming.PostgresEnumType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
 
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
@@ -19,18 +20,25 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "operation_log")
+@TypeDef(name = "pgsql_enum", typeClass = PostgresEnumType.class)
 public class OperationLogEntity implements Serializable {
 
     @EmbeddedId
     private OperationLogPk pk;
 
+    @Enumerated(EnumType.STRING)
+    @Type(type = "pgsql_enum")
     private OperationType operationType;
+
+    @Enumerated(EnumType.STRING)
+    @Type(type = "pgsql_enum")
+    private ValidationError validationError;
+
     private String accountId;
     private Long amountWithSign;
     private String currencySymbolicCode;
     private String description;
     private Long batchHash;
-    private ValidationError validationError;
     private LocalDateTime creationTime;
 
     private String spanId;
